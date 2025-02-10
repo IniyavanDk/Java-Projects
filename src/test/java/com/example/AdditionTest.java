a8bd61277057ec1d74ddfae366b83dd81b157d3c
Iniyavan Dk <95687011+IniyavanDk@users.noreply.github.com>
2025-02-10 15:45:21 +0530

    Update AdditionTest.java

--- src/test/java/com/example/AdditionTest.java
+++ src/test/java/com/example/AdditionTest.java
@@ -7,6 +7,7 @@ public class AdditionTest {
     @Test
     public void testAdd() {
         Addition addition = new Addition();
+        assertEquals(-5, addition.add(-3, -2));
         assertEquals(5, addition.add(2, 3));
         assertEquals(0, addition.add(-1, 1));
         assertEquals(-1, addition.add(-2, 1));
